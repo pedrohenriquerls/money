8d9f9f87d8f78cf726f7c8c185bcc2218655d8ec
Pedro Henrique Rodrigues <pedrohenriquerls@gmail.com>
2017-06-10 23:24:35 -0300

    improve money spec

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -3,8 +3,12 @@ RSpec.describe Money do
     expect(Money::VERSION).not_to be nil
   end
 
+  subject(:money) { Money.new(50.00, 'EUR') }
+
   describe '.inspect' do
-    it { expect(Money.new(50.00, 'EUR').inspect).to eq('50.00 EUR') }
+    it { expect(money.amount).to eq(50) }
+    it { expect(money.currency).to eq('EUR') }
+    it { expect(money.inspect).to eq('50.00 EUR') }
     it { expect(Money.new(50.10, 'EUR').inspect).to eq('50.10 EUR') }
     it { expect(Money.new(50.5524, 'EUR').inspect).to eq('50.55 EUR') }
   end
