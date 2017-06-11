e5ecf105eeee7cdfca5e0d73650d21212008380c
Pedro Henrique Rodrigues <pedrohenriquerls@gmail.com>
2017-06-10 21:46:17 -0300

    change gem configs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,11 @@
-require "bundler/setup"
-require "money"
+$LOAD_PATH.unshift File.dirname(__FILE__)
+require 'bundler/setup'
+require 'rspec'
+require 'money'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
